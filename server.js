a25ec7a1de4d943109f8af4fcb27cfa8d02fdd83
Siddhant Kulshresth <siddhantkulshresth@Siddhants-MacBook-Air.local>
2024-05-09 10:28:13 +0530

    Chanegs

--- server.js
+++ server.js
@@ -91,6 +91,11 @@ app.post('/login', async (req, res) => {
 app.get('/signup', (req, res) => {
     res.render('signup');
 });
+
+
+app.get('/forget', (req, res) => {
+    res.render('forget');
+});
 app.get('/dashboard', async(req, res) => {
 
     console.log(req.session);
